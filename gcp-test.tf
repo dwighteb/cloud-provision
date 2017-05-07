81dc7b5bea7036cfb36a28acf45086d5ffd08526
Dwight Barnette <dwighteb@gmail.com>
2017-05-07 13:25:36 -0400

    Initial commit

---
+++ gcp-test.tf
@@ -0,0 +1,33 @@
+provider "google" {
+  credentials = "${file("~/.gcloud/Terraform-my-test.json")}"
+  project     = "my-test-project-166915"
+  region      = "us-east1"
+}
+
+resource "google_compute_instance" "test-instance" {
+  name  = "test-instance1"
+  machine_type = "f1-micro"
+  zone = "us-east1-b"
+  can_ip_forward = true
+
+  tags = ["test-instance"]
+
+  disk {
+    image = "ubuntu-os-cloud/ubuntu-1604-lts"
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // ephermeral IP
+    }
+  }
+
+  metadata_startup_script = "echo hello > /test.txt"
+
+  service_account {
+    scopes = ["userinfo-email", "compute-ro", "storage-ro"]
+  }
+
+}
