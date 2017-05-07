b6a2bb1c3f178db4e730e9e93f445dbc25e8982a
Dwight Barnette <dwighteb@gmail.com>
2017-05-07 16:25:18 -0400

    Updated with test busybox server

--- gcp-test.tf
+++ gcp-test.tf
@@ -24,10 +24,25 @@ resource "google_compute_instance" "test-instance" {
     }
   }
 
-  metadata_startup_script = "echo hello > /test.txt"
+  metadata_startup_script = <<-EOF
+                            #!/bin/bash
+                            echo "Hello, World" > index.html
+                            nohup busybox httpd -f -p 8080 &
+                            EOF
+
 
   service_account {
     scopes = ["userinfo-email", "compute-ro", "storage-ro"]
   }
+}
 
+resource "google_compute_firewall" "http-8080" {
+  name    = "http-8080-allow"
+  network = "default"
+  source_ranges = ["0.0.0.0/0"]
+
+  allow {
+    protocol = "tcp"
+    ports    = ["8080"]
+  }
 }
